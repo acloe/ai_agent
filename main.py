0b3cb159cd0feb8c798fcd2985bb45c2613e9395
acloe <acloe1@gmail.com>
2025-07-25 23:28:27 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+def main():
+    import os, sys
+    from dotenv import load_dotenv
+    from google import genai
+    
+    args = sys.argv[1:]
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    
+        
+    user_prompt = " ".join(args)
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    
+    response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents=user_prompt,)
+    
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    #print("Hello from ai-agent!")
+
+
+if __name__ == "__main__":
+    main()
